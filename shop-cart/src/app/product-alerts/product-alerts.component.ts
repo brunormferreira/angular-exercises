f41a237ab7c7127c2efc5c9a2706eb6432b88bb5
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-28 14:34:38 -0300

    add: Output property

--- shop-cart/src/app/product-alerts/product-alerts.component.ts
+++ shop-cart/src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Input } from '@angular/core';
 
 @Component({
@@ -8,6 +8,7 @@ import { Input } from '@angular/core';
 })
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
+  @Output() notify = new EventEmitter();
 
   constructor() { }
 
