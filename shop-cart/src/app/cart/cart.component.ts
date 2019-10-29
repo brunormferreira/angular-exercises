c7ce9d5ba28bd3ed95011e0bf24b3cba14c4db8f
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-29 09:32:37 -0300

    add: using little reactiveForms approach

--- shop-cart/src/app/cart/cart.component.ts
+++ shop-cart/src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 import { CartService } from '../cart.service';
 
@@ -9,11 +10,26 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService,
+              private formBuilder: FormBuilder) {
+
+              this.checkoutForm = this.formBuilder.group({
+                name: '',
+                address: ''
+              });
+            }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
   }
 
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
 }
